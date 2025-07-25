ca99a08287f0b05c4b3be2fb5d6f3def8539eff6
Wade Wegner <wade.wegner@gmail.com>
2025-07-25 12:21:20 -0700

    Remove old chatbot script and update to new chatbot configuration in head.blade.php

--- resources/themes/anchor/partials/head.blade.php
+++ resources/themes/anchor/partials/head.blade.php
@@ -46,28 +46,3 @@
 @filamentStyles
 @livewireStyles
 @vite(['resources/themes/anchor/assets/css/app.css', 'resources/themes/anchor/assets/js/app.js'])
-
-
-<!-- <script async
-  src="https://yhr6mfzqcepm4uab6cgxyv3x.agents.do-ai.run/static/chatbot/widget.js"
-  data-agent-id="42d131a0-62ee-11f0-bf8f-4e013e2ddde4"
-  data-chatbot-id="YXkh3hQrnwu_qrk8xz6mtvGGOsL55mLp"
-  data-name="wave-agent Chatbot"
-  data-primary-color="#031B4E"
-  data-secondary-color="#E5E8ED"
-  data-button-background-color="#0061EB"
-  data-starting-message="Hello! How can I help you today?"
-  data-logo="/static/chatbot/icons/default-agent.svg">
-</script> -->
-
-<script async
-  src="https://qwolzpeyzsbfzwpldpk5wuje.agents.do-ai.run/static/chatbot/widget.js"
-  data-agent-id="08cd8de7-8773-11ef-bf8f-4e013e2ddde4"
-  data-chatbot-id="Z04gyYfnNc0zozj_pla1eN1TlPqhNTnr"
-  data-name="koopcast-agent Chatbot"
-  data-primary-color="#031B4E"
-  data-secondary-color="#E5E8ED"
-  data-button-background-color="#0061EB"
-  data-starting-message="Hello! How can I help you today?"
-  data-logo="/static/chatbot/icons/default-agent.svg">
-</script>
